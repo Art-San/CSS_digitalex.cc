a1742e5b6ec7b922c2c5214de70aa868aa2867b0
Aleksander <asava@sibmail.com>
2023-08-17 18:50:45 +0700

    ok

---
+++ CSS на каждый день/02. Селекторы CSS/index.js
@@ -0,0 +1,36 @@
+// Ссылка на урок
+// https://www.youtube.com/watch?v=aQkgUUmUJy4&t=83s
+
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+// prototype- Это некоторый объект с помощью которого мы рассширяем
+// свойства объектоа или классов, и с помощью его можем устраивать определеное наследование
+// в нутри JS
+//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const person = {
+//     name: 'Maxim',
+//     age: 25,
+//     greet: function () {
+//         console.log('Greet!')
+//     }
+// }
+
+// const person = new Object({
+//         name: 'Maxim',
+//         age: 25,
+//         greet: function () {
+//             console.log('Greet!')
+//         }
+//     }
+// )
+
+// Object.prototype.sayHello = function() {
+//     console.log('Hello')
+// }
+
+// const lena = Object.create(person)
+// lena.name = 'Elena'
+
+// const str = new String('I am string')
+
+// gruzz70.maska
